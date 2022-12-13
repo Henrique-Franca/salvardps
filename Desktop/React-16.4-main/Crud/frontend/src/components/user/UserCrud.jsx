f9c8922d6435a77dee511cfef966ff182f380ebc
Henrique-FrancaClear1 <henrique.francaclear1@gmail.com>
2022-12-13 11:09:03 -0300

    feat: Componente Cadastro Usuário #04

--- Desktop/React-16.4-main/Crud/frontend/src/components/user/UserCrud.jsx
+++ Desktop/React-16.4-main/Crud/frontend/src/components/user/UserCrud.jsx
@@ -19,6 +19,12 @@ export default class UserCrud extends Component {
 
     state = { ...initialState }
 
+    componentWillMount() {
+        axios(baseUrl).then(resp => {
+            this.setState({ list: resp.data })
+        })
+    }
+
     clear() {
         //referenciando ao estado inicial e limpando apenas o user
         this.setState({ user: initialState.user })
@@ -38,38 +44,38 @@ export default class UserCrud extends Component {
             })
     }
 
-    getUpdatedList(user){
+    getUpdatedList(user, add = true) {
         const list = this.state.list.filter(u => u.id !== user.id)
-        list.unshift(user)
+        if (add) list.unshift(user)
         return list
     }
 
-    updateField(event){
-        const user={...this.state.user}
+    updateField(event) {
+        const user = { ...this.state.user }
         user[event.target.name] = event.target.value
-        this.setState({user})
+        this.setState({ user })
     }
-    
+
     renderForm() {
-        return(
+        return (
             <div className="form">
                 <div className="row">
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Nome: </label>
-                            <input type="text" className="form-control" 
+                            <input type="text" className="form-control"
                                 name="name" value={this.state.user.name}
                                 onChange={e => this.updateField(e)}
-                                placeholder="Digite o nome..."/>
+                                placeholder="Digite o nome..." />
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>E-mail: </label>
-                            <input type="text" className="form-control" 
+                            <input type="text" className="form-control"
                                 name="email" value={this.state.user.email}
                                 onChange={e => this.updateField(e)}
-                                placeholder="Digite o e-mail..."/>
+                                placeholder="Digite o e-mail..." />
                         </div>
                     </div>
                 </div>
@@ -77,11 +83,11 @@ export default class UserCrud extends Component {
                 <div className="row">
                     <div className="col-12 d-flex justify-content-end">
                         <button className="btn btn-primary"
-                            onClick={e =>this.save(e)}>
+                            onClick={e => this.save(e)}>
                             Salvar
                         </button>
                         <button className="btn btn-secundary ml-2"
-                            onClick={e=> this.clear(e)}>
+                            onClick={e => this.clear(e)}>
                             Cancelar
                         </button>
 
@@ -91,10 +97,65 @@ export default class UserCrud extends Component {
         )
     }
 
+    load(user) {
+        this.setState({user})
+    }
+
+    remove(user) {
+        axios.delete(`${baseUrl}/${user.id}`).then(resp => {
+            const list = this.getUpdatedList(user, false)
+            this.setState({ list })
+        })
+    }
+
+    renderTable() {
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows() {
+        return this.state.list.map(user => {
+            //sempre que retornar um array jsx tem que retornar uma chave
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                            onClick={() => this.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                            onClick={() => this.remove(user)}>
+
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     render() {
+
         return (
             <Main {...headerProps}>
                 {this.renderForm()}
+                {this.renderTable()}
             </Main>
         )
     }
