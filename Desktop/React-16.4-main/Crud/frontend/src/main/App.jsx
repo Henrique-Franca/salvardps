59822efe84d297985ac78ee9b01395640cef76c0
Henrique-FrancaClear1 <henrique.francaclear1@gmail.com>
2022-12-13 09:36:52 -0300

    feat: Implementando Rotas (React Router)

--- Desktop/React-16.4-main/Crud/frontend/src/main/App.jsx
+++ Desktop/React-16.4-main/Crud/frontend/src/main/App.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+import { BrowserRouter } from 'react-router-dom'
 
+import Routes from './Routes'
 import Logo from '../components/tamplate/Logo'
 import Footer from '../components/tamplate/Footer'
 import Nav from '../components/tamplate/Nav'
-import Home from '../components/home/Home'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Home />
-        <Footer />
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </BrowserRouter>
+
 
